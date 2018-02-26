057d7a9d0826be6d9e96f5bbbaf0d984594d2b48
MARIA GRINAN DE MORA <Maria.Grinan@alu.uclm.es>
2018-02-26 08:57:44 +0100

    Lab assignment 1-C

--- C/Numbers.java
+++ C/Numbers.java
@@ -9,5 +9,20 @@ package concurrent_assignment1.C;
  */
 
 public class Numbers {
+    
+    public static void main(String args[])
+    {
+        R_PrintID t1= new R_PrintID(1);
+        t1.run();
+        R_PrintID t2= new R_PrintID(2);
+        t2.run();
+        R_PrintID t3= new R_PrintID(3);
+        t3.run();
+        R_PrintID t4= new R_PrintID(4);
+        t4.run();
+        R_PrintID t5= new R_PrintID(5);
+        t5.run();
+    }
 
 }
+
--- C/R_PrintID.java
+++ C/R_PrintID.java
@@ -1,4 +1,8 @@
 package concurrent_assignment1.C;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Modify class concurrent_assignment1.B.TPrintID so that it is created implementing Runnable 
  * (call the new class R_PrintID). 
@@ -8,6 +12,26 @@ package concurrent_assignment1.C;
  *
  */
 
-public class R_PrintID {
+public class R_PrintID implements Runnable{
+    java.util.Random rand = new java.util.Random();
+    int ID;
 
+    
+    public R_PrintID(int ID){
+        this.ID=ID;
+    }
+    
+    @Override
+    public void run() {
+        for (int i=1;i<=10;i++)
+        {
+            System.out.println("ID: " + i );
+            try {
+                Thread.sleep(rand.nextInt(1000));
+            } catch (InterruptedException ex) {
+                Logger.getLogger(R_PrintID.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
 }
+

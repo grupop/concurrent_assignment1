5fe5e5f939b28e9af2cf938fef8321e323a72efd
MARIA GRINAN DE MORA <Maria.Grinan@alu.uclm.es>
2018-02-26 09:04:37 +0100

    Commit in classs Reference

--- A/Reference.java
+++ A/Reference.java
@@ -1,8 +1,20 @@
 package concurrent_assignment1.A;
 
 /**
- * Get the reference of the main thread  hola  */
+ * Get the reference of the main thread    */
 
 public class Reference {
 
+     public static void main(String[] args) throws InterruptedException {
+    
+    //Thread h= Thread.currentThread();
+        
+        for(int i=1;i<=50;i++)
+        {
+            Thread.sleep(2000); //sleep the process for two second this method is static
+            System.out.println("I am the thread number: " + i);
+            
+            //Refenrece
+        }
+    }
 }

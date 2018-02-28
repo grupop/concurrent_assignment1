ded0e969b8ce6662469ad845a5fc284cecba4a14
admin <admin@192.168.1.133>
2018-03-01 00:23:15 +0100

    Do  Numbers(C) and R_print class

--- C/R_PrintID.java
+++ C/R_PrintID.java
@@ -12,15 +12,30 @@ import java.util.logging.Logger;
  *
  */
 
+//Here implements interface Runneable -->  because we modificated method run, 
+                                         //and this interfaz have yet the capacity to create other thread
+
 public class R_PrintID implements Runnable{
+    
+    //Declare random variable to identify each thread 
+    
     java.util.Random rand = new java.util.Random();
     int ID;
 
-    
+    //This is a constructor, he assign one pharmaters whith our id declare
     public R_PrintID(int ID){
         this.ID=ID;
     }
     
+    
+    /*
+     * In this case we do the method run, implementing the runneable interface and defining the run function  of this interface.
+      - The loop sleeps the thread a random time between 0 and 1
+      -For it, we use the method sleep to sleep a thread for the specified amount of time
+      -If you sleep a thread for the specified time,another thread is exejute and so on.
+      -A Logger object is used to log messages for a specific system or application component.
+   
+   */
     @Override
     public void run() {
         for (int i=1;i<=10;i++)
